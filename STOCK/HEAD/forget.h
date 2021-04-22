07ec9b518416ca6163c56bb01ad5fa02c4adc437
shenlei515 <69578089+shenlei515@users.noreply.github.com>
2021-04-22 17:42:22 +0800

    Add files via upload

---
+++ STOCK/HEAD/forget.h
@@ -0,0 +1,7 @@
+#ifndef _fogetpass_h
+#define _forgetpass_h
+
+void fgpass_drawer();
+int s_fgpass();
+
+#endif
---
+++ STOCK/HEAD/visual.h
@@ -0,0 +1,10 @@
+#ifndef _visualize_h
+#define _visualize_h
+
+void judge_visual(int state3,int start,int end);
+int judge_station(int state3,int *start,int *end);
+void choice_switch(int state3,int botton);
+void passenger_list(int checi);
+void draw_seat(int x,int y,int color);
+
+#endif
